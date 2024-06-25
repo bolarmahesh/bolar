187bf01919f575f2c5f80c09233b73c30d82ad5d
bolarmahesh <mahesh.bolar@netapp.com>
2024-06-25 18:54:12 -0400

    Update waflsusp.sh

--- waflsusp.sh
+++ waflsusp.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
-ngsh -c "set d;date;node run -node cluster1-01 -command wafl_susp -w;sleep 3;\
-node run -node cluster1-01 -command wafl_susp -w" >> /mroot/etc/crash/"$(date +"%Y_%m_%d_%I_%M_%p")"-waflsusp.txt & \
+ngsh -c "set d;date;node run -node * -command wafl_susp -w;sleep 10;\
+node run -node * -command wafl_susp -w;sleep 10;\
+node run -node * -command wafl_susp -w" >> /mroot/etc/crash/"$(date +"%Y_%m_%d_%I_%M_%p")"-waflsusp.txt & \
 \
