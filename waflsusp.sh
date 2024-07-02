2c076010fb656f8cda70a337974c0769008bdbfe
bolarmahesh <mahesh.bolar@netapp.com>
2024-07-02 13:01:19 -0400

    Update waflsusp.sh

--- waflsusp.sh
+++ waflsusp.sh
@@ -4,3 +4,4 @@ node run -node * -command wafl_susp -w;sleep 60;\
 node run -node * -command wafl_susp -w;sleep 60;\
 node run -node * -command wafl_susp -w" >> /mroot/etc/crash/"$(date +"%Y_%m_%d_%I_%M_%p")"-waflsusp.txt & \
 \
+ngsh -c "set d;date;echo "collecting 5 minute data.....Wait untill script collection is complete....Do not pres any keys";sleep 300;date"
